b813c037c0fe28bf8819e7d36b7c03c92cf931d3
TheDeafOne <woodburnkb20@gcc.edu>
2023-05-26 21:58:43 -0500

    testing pre calendar styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Calendar from "@/components/Calendar/Calendar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
-        hello
+        <Calendar />
       </div>
     </main>
   )
---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,9 @@
+const Calendar = () => {
+    return (
+        <div className="bg-black h-px-200">
+            hello
+        </div>
+    )
+}
+
+export default Calendar;
